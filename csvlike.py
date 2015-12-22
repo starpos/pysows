60e0324f1deb09226b4cb1e3efbb7cd95b5fc003
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2015-12-22 18:38:53 +0900

    csvlike: change indent.

--- csvlike.py
+++ csvlike.py
@@ -4,69 +4,71 @@
 from relation import Schema, Relation
 
 class CsvLike(Relation):
-  """
-  CSV-like data.
-
-  1st line must be header starting by '#'
-  where column names are listed separated by the separator.
-
-  """
-  def __init__(self, lineGenerator, sep=None, schema=None, reuse=True):
     """
-    lineGenerator :: generator(str)
-      CSV-like data.
-    sep :: str
-       separator for str.split().
-    schema :: Schema
-       Use the schema.
-       Assume there is no header.
+    CSV-like data.
+
+    1st line must be header starting by '#'
+    where column names are listed separated by the separator.
 
     """
-    if schema is None:
-      schemaLine = lineGenerator.next().rstrip()
-      schema = Schema.parse(schemaLine, sep=sep)
+    def __init__(self, lineGenerator, sep=None, schema=None, reuse=True):
+        """
+        lineGenerator :: generator(str)
+          CSV-like data.
+        sep :: str
+           separator for str.split().
+        schema :: Schema
+           Use the schema.
+           Assume there is no header.
 
-    def getRawRecGenerator():
-      for line in lineGenerator:
-        yield schema.parseValues(line.rstrip().split(sep))
-    Relation.__init__(self, schema, getRawRecGenerator(), reuse=reuse)
+        """
+        if schema is None:
+            schemaLine = lineGenerator.next().rstrip()
+            schema = Schema.parse(schemaLine, sep=sep)
 
-  def save(self, outFile, sep='\t'):
-    """
-    Save the relation.
+        def getRawRecGenerator():
+            for line in lineGenerator:
+                yield schema.parseValues(line.rstrip().split(sep))
+        Relation.__init__(self, schema, getRawRecGenerator(), reuse=reuse)
 
-    outFile :: file
-    return :: None
+    def save(self, outFile, sep='\t'):
+        """
+        Save the relation.
 
-    """
-    for s in self.showG(sep=sep):
-      outFile.write(s)
-      outFile.write('\n')
+        outFile :: file
+        return :: None
+
+        """
+        for s in self.showG(sep=sep):
+            outFile.write(s)
+            outFile.write('\n')
+
 
 def sampleCsvLike():
-  """
-  sample code.
+    """
+    sample code.
 
-  """
-  import sys
-  import StringIO
-  def lg():
-    yield '#c1 c2::String c3::Integer c4::Decimal c5::Float'
-    yield '1 2 3 4 5'
-    yield '2 3 4 5 6'
-  rel = CsvLike(lg())
-  print rel.schema()
-  for raw in rel.getL():
-    print raw
-  rel.save(sys.stdout)
-  print rel.show(sep=' '),
-  fo = StringIO.StringIO()
-  rel.save(fo)
-  fi = StringIO.StringIO(fo.getvalue())
-  fo.close()
-  rel = CsvLike(fi)
-  print rel,
-  fi.close()
+    """
+    import sys
+    import StringIO
+    def lg():
+        yield '#c1 c2::String c3::Integer c4::Decimal c5::Float'
+        yield '1 2 3 4 5'
+        yield '2 3 4 5 6'
+    rel = CsvLike(lg())
+    print rel.schema()
+    for raw in rel.getL():
+        print raw
+    rel.save(sys.stdout)
+    print rel.show(sep=' '),
+    fo = StringIO.StringIO()
+    rel.save(fo)
+    fi = StringIO.StringIO(fo.getvalue())
+    fo.close()
+    rel = CsvLike(fi)
+    print rel,
+    fi.close()
+
 
 if __name__ == '__main__':
-  sampleCsvLike()
+    sampleCsvLike()
